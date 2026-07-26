3b6098284cfa018da79c28c4cf1f9b727231df49
Shivsai Jagadale <shivsai1396@gmail.com>
2026-07-26 20:26:28 +0530

    08_Modules

---
+++ 03_import datetime.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+now = datetime.now()
+
+print("Current Date and Time:", now)
+print("Year:", now.year)
+print("Month:", now.month)
+print("Day:", now.day)
+print("Hour:", now.hour)
+print("Minute:", now.minute)
