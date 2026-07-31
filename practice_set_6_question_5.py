b02cea09c8241e94948d631a43bead4cea21a8ed
Shivsai Jagadale <shivsai1396@gmail.com>
2026-07-31 20:19:28 +0530

    09_Practice_Sets_(Practice_Set_06)

---
+++ practice_set_6_question_5.py
@@ -0,0 +1,7 @@
+l=["Shiv","Shivsai","Alok","Akhilesh","Satyam","Shreyas"]
+name=input("Enter your name:")
+
+if(name in l):
+    print("Your name is present in the list")
+else:
+    print("Your name is not present in the list")
