75c49cd3dcb1475ce76b148b0d01b391d8a3394e
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-03 22:04:36 +0530

    09_Practice_Sets_(Practice_Set_09)

---
+++ practice_set_9_question_5.py
@@ -0,0 +1,7 @@
+words=["Donkey","Ganda","bad"]
+with open("file.txt") as f:
+    content=f.read()
+for word in words:
+    content=content.replace(word,"#"*len(word))
+with open("file.txt","w") as f:
+    f.write(content)       
