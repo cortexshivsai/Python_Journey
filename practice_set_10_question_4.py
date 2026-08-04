6c1a0699eab10bce05b31d4083e53718faecc46a
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-04 20:22:22 +0530

    09_Practice_Sets_(Practice_Set_10)

---
+++ practice_set_10_question_4.py
@@ -0,0 +1,19 @@
+class calculator:
+    
+    def __init__(self,n):
+        self.n=n
+    def square(self):
+        print(f"The square is {self.n*self.n}")    
+    def cube(self):
+        print(f"The cube is {self.n*self.n*self.n}")  
+    def sqrt(self):
+        print(f"The square root is {self.n**1/2}")
+    @staticmethod
+    def hello():
+        print("Hello User")
+a=calculator(4)
+a.hello()
+a.square()
+a.cube()
+a.sqrt()  
+      
