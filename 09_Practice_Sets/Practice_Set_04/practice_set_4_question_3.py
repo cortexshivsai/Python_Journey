26de01b2f1896f2fd9a691423a26ab1343e13008
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 22:19:18 +0530

    Practice_Set_04

---
+++ 09_Practice_Sets/Practice_Set_04/practice_set_4_question_3.py
@@ -0,0 +1,4 @@
+#W.A.P to check that a tuple type cannot be changed in python
+t=(123,456,"Shiv",6969,"Sai",916)
+t[2]="Shivsai"
+print(t)
---
+++ 09_Practice_Sets/Practice_Set_04/practice_set_4_question_4.py
@@ -0,0 +1,4 @@
+#W.A.P to sum a list with 4 numbers
+
+list1=[1223,456]
+print(sum(list1)) 
