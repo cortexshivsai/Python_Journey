d4d92c4ac2f968aefa3ffe77548a1ec480d266c2
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:17:13 +0530

    Delete practice_set_9_question_9.py

--- practice_set_9_question_9.py
+++
@@ -1,9 +0,0 @@
-with open("shiv22.txt") as f:
-    content=f.read()
-with open("shiv223.txt") as f:
-    content2=f.read()  
-
-if( content==content2 ) :
-    print("Yes these files are identical...")
-else:
-    print("NO these files are not identical!")          
