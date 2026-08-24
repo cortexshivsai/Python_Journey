82f196336ce81588650f126e214f8ad59f1f48dc
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:25:03 +0530

    Delete 07_import calculator_own_module.py

--- 07_import calculator_own_module.py
+++
@@ -1,6 +0,0 @@
-from calculator import add,subtract,multiply,divide
-
-print("Addition:",add(10, 5))
-print("Subtraction:",subtract(10, 5))
-print("Multiplication:",multiply(10, 4))
-print("Division:",divide(10, 5))
