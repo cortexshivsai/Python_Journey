d5a7da585d516d055dcc97c22e54f47379ea1936
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:29:56 +0530

    Delete 03_If_elif_else_ladder.py

--- 03_If_elif_else_ladder.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter your age:"))
-if(a>=18):
-    print("You are eligible to vote")
-elif(a<0):
-    print("Invalid age")
-elif(a==0):
-    print("Invalid Age")
-else:
-    print("You are not eligible to vote")
