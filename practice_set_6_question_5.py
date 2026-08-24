00c653ae3cf92508ea9fbbca75a629ab21761a80
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:14:13 +0530

    Delete practice_set_6_question_5.py

--- practice_set_6_question_5.py
+++
@@ -1,7 +0,0 @@
-l=["Shiv","Shivsai","Alok","Akhilesh","Satyam","Shreyas"]
-name=input("Enter your name:")
-
-if(name in l):
-    print("Your name is present in the list")
-else:
-    print("Your name is not present in the list")
