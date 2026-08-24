a4a80814238a071b54bb56ff65748e74e516866d
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:30:18 +0530

    Delete 04_Exception_raising.py

--- 04_Exception_raising.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-
-if(b==0):
-    raise ZeroDivisionError("Can't divide by zero...")
-else:
-    print(f"Division is : {a/b}")
-
-    
