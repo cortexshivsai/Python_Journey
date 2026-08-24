8adf7381010c3c1a2f20c45c061a516cda300c56
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:16:10 +0530

    Delete practice_set_9_question_5.py

--- practice_set_9_question_5.py
+++
@@ -1,7 +0,0 @@
-words=["Donkey","Ganda","bad"]
-with open("file.txt") as f:
-    content=f.read()
-for word in words:
-    content=content.replace(word,"#"*len(word))
-with open("file.txt","w") as f:
-    f.write(content)       
