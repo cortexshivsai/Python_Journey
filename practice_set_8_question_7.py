01baabf02b194b1ee39538ab18f9f72bd95a5c51
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:14:55 +0530

    Delete practice_set_8_question_7.py

--- practice_set_8_question_7.py
+++
@@ -1,8 +0,0 @@
-def rem(l,word):
-    n=[]
-    for item in l:
-        if not(item==word):
-            n.append(item.strip(word))
-    return n
-l=["Shiv","Sai","Shivsai","Shubh"]  
-print(rem(l,"ai"))      
