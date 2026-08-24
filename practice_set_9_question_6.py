b350c4b518e356b45d69727ff597f123f003dce5
Shivsai Jagadale <shivsai1396@gmail.com>
2026-08-24 19:16:26 +0530

    Delete practice_set_9_question_6.py

--- practice_set_9_question_6.py
+++
@@ -1,8 +0,0 @@
-with open("new.txt") as f:
-    content=f.read()
-
-
-if("Python" in content):
-    print("Python is present in the content!")    
-else:
-    print("Python is not present in the content!")    
